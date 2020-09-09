8c0e97643859f8e99607ddad634fde2d341c2675
ymyk-uiai <u8dzw47ty@protonmail.com>
2020-09-09 20:18:34 +0900

    add provider and src directory

---
+++ src/Providers/EruptServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Erupt\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class EruptProvider extends ServiceProvider
+{
+  public function register()
+  {
+    // 
+  }
+
+  public function boot()
+  {
+    // 
+  }
+}
